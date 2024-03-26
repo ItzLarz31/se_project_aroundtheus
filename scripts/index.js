a1a62920c1be7806deeda144f7c2d59aa6324b85
ItzLarz31 <larz9889@hotmail.com>
2024-03-26 15:58:32 -0400

    Refactored modal open and close functions to call for modal argument so I can make them universal to prevent repeating code

--- scripts/index.js
+++ scripts/index.js
@@ -34,9 +34,12 @@ const initialCards = [
 /*                                  Elements                                  */
 /* -------------------------------------------------------------------------- */
 
+// Content
 const content = document.querySelector(".content");
 const profileEditButton = content.querySelector(".profile__edit-button");
 const profileTitle = content.querySelector(".profile__title");
+
+//Profile Modal
 const profileDescription = content.querySelector(".profile__description");
 const profileEditModal = document.querySelector("#edit-modal");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
@@ -46,10 +49,12 @@ const profileModalDescription = profileEditModal.querySelector(
   "#profile-description-input"
 );
 
+// Card List
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
+// Card Modal
 const cardModal = document.querySelector("#card-modal");
 const cardEditForm = cardModal.querySelector(".modal__form");
 const cardCloseButton = cardModal.querySelector(".modal__close");
@@ -61,39 +66,27 @@ const cardAddButton = document.querySelector(".profile__add-button");
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-function openProfileModal() {
-  profileEditModal.classList.add("modal_opened");
-  profileModalName.value = profileTitle.textContent;
-  profileModalDescription.value = profileDescription.textContent;
+function openModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
-function closeProfileModal() {
-  profileEditModal.classList.remove("modal_opened");
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
 }
 
 function saveProfileModalInput(evt) {
   evt.preventDefault();
   profileTitle.textContent = profileModalName.value;
   profileDescription.textContent = profileModalDescription.value;
-  closeProfileModal();
+  closeModal(profileEditModal);
 }
 
-function openCardModal() {
-  cardModal.classList.add("modal_opened");
-  cardModalTitle.value = profileTitle.textContent;
-  cardModalUrl.value = profileDescription.textContent;
-}
-
-function closeCardModal() {
-  cardModal.classList.remove("modal_opened");
-}
-
-function saveCardModalInput(evt) {
-  evt.preventDefault();
-  profileTitle.textContent = profileModalName.value;
-  profileDescription.textContent = profileModalDescription.value;
-  closeCardModal();
-}
+// function saveCardModalInput(evt) {
+//   evt.preventDefault();
+//   profileTitle.textContent = profileModalName.value;
+//   profileDescription.textContent = profileModalDescription.value;
+//   closeCardModal();
+// }
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
@@ -110,13 +103,21 @@ function getCardElement(cardData) {
 /*                               Event Listeners                               */
 /* -------------------------------------------------------------------------- */
 
-profileEditButton.addEventListener("click", openProfileModal);
-profileCloseButton.addEventListener("click", closeProfileModal);
+// Profile Modal
+profileEditButton.addEventListener("click", () => {
+  openModal(profileEditModal);
+  profileModalName.value = profileTitle.textContent;
+  profileModalDescription.value = profileDescription.textContent;
+});
+profileCloseButton.addEventListener("click", () =>
+  closeModal(profileEditModal)
+);
 profileEditForm.addEventListener("submit", saveProfileModalInput);
 
-cardAddButton.addEventListener("click", openCardModal);
-cardCloseButton.addEventListener("click", closeCardModal);
-cardEditForm.addEventListener("submit", saveCardModalInput);
+// Card Modal
+cardAddButton.addEventListener("click", () => openModal(cardModal));
+cardCloseButton.addEventListener("click", () => closeModal(cardModal));
+// cardEditForm.addEventListener("submit", saveCardModalInput);
 
 /* -------------------------------------------------------------------------- */
 /*                                  For Loops                                  */
